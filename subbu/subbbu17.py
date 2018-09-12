91f6b45ec10889fbca779e5d24499ab363388cba
Ramkumarsubramaniyan <36755444+Ramkumarsubramaniyan@users.noreply.github.com>
2018-09-12 14:33:47 +0530

    Update subbbu17.py

--- subbu/subbbu17.py
+++ subbu/subbbu17.py
@@ -1,7 +1,7 @@
-a=[]
+c=[]
 n=int(input("Enter number of elements:"))
 for i in range(1,n+1):
     b=int(input(" "))
-    a.append(b)
-a.sort()
-print("Largest element is:",a[n-1])
+    c.append(b)
+c.sort()
+print("Largest element is:",c[n-1])
